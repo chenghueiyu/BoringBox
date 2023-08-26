40a9afa34594280c546cf0f92c6229063f704ebd
chenghueiyu <143241140+chenghueiyu@users.noreply.github.com>
2023-08-26 11:43:15 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+edubitMotors.setServoPosition(ServoChannel.S1, 170)
+let num = 0
+basic.showIcon(IconNames.Asleep)
+basic.forever(function () {
+    if (true) {
+    	
+    } else {
+    	
+    }
+})
